f30293b13189739cc7b1db7c509657b77e0be8b8
nadendlaram <108746502+nadendlaram@users.noreply.github.com>
2023-01-05 15:52:33 +0530

    Update DockerBuild.sh

--- scripts/DockerBuild.sh
+++ scripts/DockerBuild.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-cd /home/ec2-user
-mkdir ram
-cd ram
-touch ram1 ram2 ram3 ram4 ram5 
-ls
+aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin 412937381715.dkr.ecr.ap-northeast-1.amazonaws.com
+docker pull 412937381715.dkr.ecr.ap-northeast-1.amazonaws.com/spring-images:latest
 
+#docker stop nodejs-1 || true && docker rm nodejs-1 || true
+docker run -p 80:80 --name tomcat-1 412937381715.dkr.ecr.ap-northeast-1.amazonaws.com/spring-images:latest
+docker run -p 81:80 --name tomcat-2 412937381715.dkr.ecr.ap-northeast-1.amazonaws.com/spring-images:latest
+docker run -p 82:80 --name tomcat-3 412937381715.dkr.ecr.ap-northeast-1.amazonaws.com/spring-images:latest
