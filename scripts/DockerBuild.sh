5c857aa76da74ba03cc9c9b04314f03836dafc4b
nadendlaram <108746502+nadendlaram@users.noreply.github.com>
2023-01-05 17:06:36 +0530

    Update DockerBuild.sh

--- scripts/DockerBuild.sh
+++ scripts/DockerBuild.sh
@@ -2,7 +2,7 @@
 aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin 412937381715.dkr.ecr.ap-northeast-1.amazonaws.com
 docker pull 412937381715.dkr.ecr.ap-northeast-1.amazonaws.com/spring-images:latest
 
-#docker stop tomcat-1 || true && docker rm tomcat-1 || true
+docker stop tomcat-1 || true && docker rm tomcat-1 || true
 docker run -p 8080:8080 --name tomcat-1 412937381715.dkr.ecr.ap-northeast-1.amazonaws.com/spring-images:latest
 docker stop tomcat-2 || true && docker rm tomcat-2 || true
 docker run -p 8081:8080--name tomcat-2 412937381715.dkr.ecr.ap-northeast-1.amazonaws.com/spring-images:latest
