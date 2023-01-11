b7ae7ca4b4decab83d759e362527ece94dda55ce
nadendlaram <108746502+nadendlaram@users.noreply.github.com>
2023-01-11 17:29:20 +0530

    Update DockerBuild.sh

--- scripts/DockerBuild.sh
+++ scripts/DockerBuild.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin 412937381715.dkr.ecr.ap-northeast-1.amazonaws.com
-docker pull 412937381715.dkr.ecr.ap-northeast-1.amazonaws.com/spring-images:latest
+docker pull 412937381715.dkr.ecr.ap-northeast-1.amazonaws.com/spring-images@sha256:665cbccfcbd5923c89d1a96400302e5618b718112a17e2829afc03903447bd18
 
 docker stop tomcat-1 || true && docker rm tomcat-1 || true
-docker run --name tomcat-1 412937381715.dkr.ecr.ap-northeast-1.amazonaws.com/spring-images:latest
+docker run --name tomcat-4 412937381715.dkr.ecr.ap-northeast-1.amazonaws.com/spring-images@sha256:665cbccfcbd5923c89d1a96400302e5618b718112a17e2829afc03903447bd18
 docker stop tomcat-2 || true && docker rm tomcat-2 || true
-docker run --name tomcat-2 412937381715.dkr.ecr.ap-northeast-1.amazonaws.com/spring-images:latest
+docker run --name tomcat-5 412937381715.dkr.ecr.ap-northeast-1.amazonaws.com/spring-images@sha256:665cbccfcbd5923c89d1a96400302e5618b718112a17e2829afc03903447bd18
 docker stop tomcat-3 || true && docker rm tomcat-3 || true
-docker run --name tomcat-3 412937381715.dkr.ecr.ap-northeast-1.amazonaws.com/spring-images:latest
+docker run --name tomcat-6 412937381715.dkr.ecr.ap-northeast-1.amazonaws.com/spring-images@sha256:665cbccfcbd5923c89d1a96400302e5618b718112a17e2829afc03903447bd18
